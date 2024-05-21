2e86ce0edeab7516842095863b518d7c37f8cc9b
abdo-hm02 <abdessamad2002@gmail.com>
2024-05-21 05:21:56 +0200

    plans service redo

---
+++ plans service/plans-service/src/main/java/com/plansservice/plans/service/PlansServiceApplication.java
@@ -0,0 +1,13 @@
+package com.plansservice.plans.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlansServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlansServiceApplication.class, args);
+	}
+
+}
